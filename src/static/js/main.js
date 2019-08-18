f1ebeb509fd693ecde094443d0509bce6a0bb730
Gougeaway <zenkov1986@gmail.com>
2019-08-18 22:22:28 +0700

    done

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,3 +1,52 @@
 $(document).ready(function () {
-	$('.slider').slick();
+
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+		direction: 'horizontal',
+		loop: true,
+		pagination: {
+			el: '.swiper-pagination',
+		},
+		navigation: {
+			nextEl: '.swiper-button-next',
+			prevEl: '.swiper-button-prev',
+		}
+    
+  });
+
+
+
+
+
+
+	//$('.slider').slick();
 });
+
+
+
+
+
+/*
+var mySwiper = new Swiper ('.swiper-container', {
+    // Optional parameters
+    direction: 'vertical',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  })
+  */
